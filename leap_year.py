e6ed8894c0b4162f384f0e2c87b78629a7ce3dc6
xkeshav29 <x.keshav29@gmail.com>
2022-06-19 17:03:41 +0530

    leap year

---
+++ leap_year.py
@@ -0,0 +1,26 @@
+def isLeapYear(year: int) -> bool:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+        else:
+            return False
+    else:
+        if year % 4 == 0:
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    years: list[int] = [2020, 2021, 2023]
+    #results: list[bool] = [print(f"{years[index]} : {isLeapYear(year)}")
+    # for index, year in enumerate(years)]
+    results: list[bool] = [print(f"{year} is leap year") if isLeapYear() 
+    else print(f"{year} is not leap year") for year in years]
+    
+    # print(f"{year} is a Leap Year" if isLeapYear(year) else f"{year} is not a Leap Year")
+    # year: int = 2021
+    # print(f"{year} is a Leap Year" if isLeapYear(year) else f"{year} is not a Leap Year")
+    # year: int = 2022
+    # print(f"{year} is a Leap Year" if isLeapYear(year) else f"{year} is not a Leap Year")
+
