6d4f6d95d5c758124ff74e937d0ffbb46cf249e9
kr-devesh-0246 <devesh2462003@gmail.com>
2022-06-20 12:53:25 +0530

    new file named xpowery.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # WAP to calculate x^y where x and y are positive integers greater than 1
 # you should not use multiply(*) or power function. only add(+) is allowed
 def multiply(a: int, b: int) -> int:
-
+    res = 0
     for i in range(b):
         print(f"res : {res}")
         res += a
@@ -9,7 +9,7 @@ def multiply(a: int, b: int) -> int:
 
 
 def power(a: int, b: int) -> int:
-    #   apowerb = a*a*a*a....b  times
+    #   a^b = a*a*a*a....b  times
     res = 1
     for i in range(b):
         res = multiply(res, a)
