2d149785f91e2f9e74719a3a4a6cec1403a5775e
kr-devesh-0246 <devesh2462003@gmail.com>
2022-06-20 12:16:37 +0530

    power function made

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # WAP to calculate x^y where x and y are positive integers greater than 1
 # you should not use multiply(*) or power function. only add(+) is allowed
 def multiply(a: int, b: int) -> int:
-    # 2*3 = 2+2+2
-    # a*b=a+a+a... b times
-    res = 0
+
     for i in range(b):
+        print(f"res : {res}")
         res += a
     return res
 
 
+def power(a: int, b: int) -> int:
+    #   apowerb = a*a*a*a....b  times
+    res = 1
+    for i in range(b):
+        res = multiply(res, a)
+
+    return res
+
+
 if __name__ == "__main__":
-    print(multiply(2, 3))
+    print(power(2, 3))
