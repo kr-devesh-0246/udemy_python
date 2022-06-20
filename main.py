37d4bce3857058b8551151eea386f9beb3043acc
Keshav Kumar <x.keshav29@gmail.com>
2022-06-20 12:29:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,30 @@ def power(a: int, b: int) -> int:
 
 if __name__ == "__main__":
     print(power(2, 3))
+
+    
+'''
+Dry Run:
+
+
+power(2, 3)
+
+     Power                Multiply
+  a  b.    Res.            A.  B. res
+  2. 3        1            1.   2.  0         
+multiply(1, 2)                    0+a = 0+1 = 1
+                                  1+a = 1+1 = 2
+
+                res =2
+multiply(2, 2)
+                           2.  2.   0 
+                                    0+a = 0+2 = 2
+                                    2+a = 2+2 = 4
+                Res = 4
+multiply(4, 2)         
+				4 2   0  
+                                         res = 0 + a = 0 + 4
+                                         res = 4 + a = 4 + 4 = 8
+8
+'''
+
